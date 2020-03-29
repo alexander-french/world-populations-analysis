479a4ab91c083826d0cf563f296d85ecb5052579
Alexander French <alexander.french.uk@gmail.com>
2020-03-29 13:18:01 +0100

    first two queries completed

--- population_queries.sql
+++ population_queries.sql
@@ -1,10 +1,15 @@
 -- How many entries in the database are from Africa?
-
-
+SELECT COUNT(continent) AS 'Total in Africa'
+FROM countries
+WHERE continent = 'Africa';
 
 -- What was the total population of Oceania in 2005?
-
-
+SELECT SUM(population) AS 'Oceania Population (2005)'
+FROM countries
+JOIN population_years
+    ON countries.id = population_years.country_id
+WHERE continent = 'Oceania'
+    AND year = 2005;
 
 -- What is the average population of countries in South America in 2003?
 
